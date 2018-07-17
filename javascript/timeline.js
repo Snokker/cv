124db408a004c1f0cf2d04a155336398bb868806
Lisa <lisagameart@gmail.com>
2018-07-17 16:58:06 +0200

    skills aangepast, deze zijn goed nu

--- javascript/timeline.js
+++ javascript/timeline.js
@@ -4,11 +4,13 @@
  // Create a DataSet (allows two way data-binding)
  var items = new vis.DataSet([
      {id: 1, content: 'de dag dat mijn moeder spijt kreeg', start: '1994-03-29'},
-     {id: 2, content: 'basis school', start: '1998-01-01'},
-     {id: 3, content: 'middelbare school vwo', start: '2006-01-01'},
+     {id: 2, content: 'basisschool', start: '1998-01-01'},
+     {id: 3, content: 'middelbareschool vwo', start: '2006-01-01'},
      {id: 4, content: 'HKU gameart', start: '2013-01-01'},
      {id: 5, content: 'Informatica uu', start: '2017-01-01'},
      {id: 6, content: 'stage gamious', start: '2015-01-01'},
+     {id: 7, content: 'verscheidende bijbaantjes', start: '2010-01-01'},
+     {id: 8, content: 'gestart met boulderen', start: '2017-01-01'},
  ]);
 
  // Configuration for the Timeline
@@ -18,7 +20,7 @@
      min:'1950',
      max:'2050',
      timeAxis: {scale: 'year', step: 5},
-     minHeight: 300,
+     minHeight: 350,
  };
 
  // Create a Timeline
