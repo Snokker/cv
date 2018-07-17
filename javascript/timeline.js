7e917fb8f0a809169a9d0b8e2c0c5fe21dbc84e2
Lisa <lisagameart@gmail.com>
2018-07-17 14:33:24 +0200

    code netter gemaakt en volgense mij skill balk toegevoegd

---
+++ javascript/timeline.js
@@ -0,0 +1,25 @@
+ // DOM element where the Timeline will be attached
+ var container = document.getElementById('visualization');
+
+ // Create a DataSet (allows two way data-binding)
+ var items = new vis.DataSet([
+     {id: 1, content: 'de dag dat mijn moeder spijt kreeg', start: '1994-03-29'},
+     {id: 2, content: 'basis school', start: '1998-01-01'},
+     {id: 3, content: 'middelbare school vwo', start: '2006-01-01'},
+     {id: 4, content: 'HKU gameart', start: '2013-01-01'},
+     {id: 5, content: 'Informatica uu', start: '2017-01-01'},
+     {id: 6, content: 'stage gamious', start: '2015-01-01'},
+ ]);
+
+ // Configuration for the Timeline
+ var options = {
+     start:'1994',
+     end:'2018',
+     // min:'1994',
+     // max:'2018',
+     timeAxis: {scale: 'year', step: 5},
+     minHeight: 300,
+ };
+
+ // Create a Timeline
+ var timeline = new vis.Timeline(container, items, options);
